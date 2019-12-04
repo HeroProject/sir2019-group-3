f00d5eda6729de3cad84a014c17887b654673eb1
Srinivas1043 <sm1043@gmail.com>
2019-12-04 16:16:52 +0100

    Add files via upload

--- SampleApplication.py
+++ SampleApplication.py
@@ -4,20 +4,9 @@ import random
 
 
 class SampleApplication(Base.AbstractApplication):
-    # def __init__(self):
-    #     Base.AbstractApplication.__init__(self)
-    #     self.name = None
-    #     self.age = None
-    #     self.scenario_choice = None
-    #
-    #     self.entities = {"answer_name": "name",
-    #                      "answer_age": "age",
-    #                      "pick_scenario": "scenario_choice"}
-
     def main(self):
         
-        #
-        # print(self.Questions.keys())
+        
         self.name = ""
         self.age = ""
         self.scenario_choice = ""
@@ -41,15 +30,16 @@ class SampleApplication(Base.AbstractApplication):
              3:["How are you doing today?","How are things going?","How are you feeling?"],
              4: "These are the situations we can practice: 1: checking out at a supermarket, 2: meeting new people, 3: ordering food at a restaurant, 4: having a job interview, 5: expressing opinions, 6: inviting others to do something together. Which number would you like to practice?"}
 
-        self.Answers = {1:["Nice name!" , self.name + "I like that!"],
+        self.Answers = {1:["Nice name!"  + self.name +  "I like that!"],
            2:["I'm 24", "I'm 24 years old"],
            3:["My day was okay so far. A customer at the grocery shop I work at was being difficult and wanted to speak to the manager, but I managed to solve the problem after all!", "My day was okay so far. I had a presentation for a course of mine. I was really nervous, but people said it went really well! I’m relieved."],
            4:["Nice! So scenario" + self.scenario_choice + "We will practice that next session", "Great! So scenario" + self.scenario_choice + "I look forward to practicing that next time", "Sounds good! So scenario" + self.scenario_choice + "I can’t wait to practice it with you", "Alright! We will practice scenario" + self.scenario_choice + "the next session then. I bet you’ll do great"]}
         
         
+        a = list(self.Questions.keys())[0]
+        b = self.Questions[a]
         
-        
-        
+        print(random.choice(b))
         
         
         
@@ -65,9 +55,10 @@ class SampleApplication(Base.AbstractApplication):
  
         # Make the robot ask the question, and wait until it is done speaking
         while self.Questions.keys != "":
-            
+            list_key_init =  list(self.Questions.keys)
+            key_init = list_key_init[0]
             self.speechLock = Semaphore(0)
-            self.sayAnimated('Hello, what is your name?')
+            self.sayAnimated(random.choice(self.Questions[key_init]))
             self.speechLock.acquire()
  
             # Listen for an answer for at most 5 seconds
@@ -101,14 +92,8 @@ class SampleApplication(Base.AbstractApplication):
             self.gestureLock.release()
  
     def onAudioIntent(self, *args, intentName):
-        """
-        get the input of the user, get the attribute this input should
-        be assigned to, set the attribute
-        """
-        if intentName in self.entities and len(args) > 0:
-            user_input = args[0]
-            attribute = self.entities["answer_name"]
-            setattr(self, attribute, user_input)
+        if intentName == 'answer_name' and len(args) > 0:
+            self.name = args[0]
             self.nameLock.release()
 
 
