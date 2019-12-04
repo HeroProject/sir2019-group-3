848cffd63c5d07962c801d3e878c405b0229c196
robinchelsea <57752523+robinchelsea@users.noreply.github.com>
2019-12-04 14:40:24 +0100

    Update Q&A.py

--- Q&A.py
+++ Q&A.py
@@ -1,8 +1,10 @@
 
 Questions = {1:["Hi, my name is" + self.robotname + "what is your name?", "Hello,  I'm" + self.robotname + "How can I call you?"],
-             2:["How are you doing today?","How are things going?","How are you feeling?"],
-             3: "These are the situations we can practice: 1: checking out at a supermarket, 2: meeting new people, 3: ordering food at a restaurant, 4: having a job interview, 5: expressing opinions, 6: inviting others to do something together. Which number would you like to practice?"}
+             2:["How old are you?", "What is your age?"],
+             3:["How are you doing today?","How are things going?","How are you feeling?"],
+             4: "These are the situations we can practice: 1: checking out at a supermarket, 2: meeting new people, 3: ordering food at a restaurant, 4: having a job interview, 5: expressing opinions, 6: inviting others to do something together. Which number would you like to practice?"}
 
-Answers = {1:["Nice name! I'm 24, how old are you?", self.name + "I like that! I'm 24, what about you?", self.name + "I like that, how old are you? I'm 24", "Nice name! what's your age? I'm 24!"],
+Answers = {1:["Nice name!", self.name + "I like that!"],
+           2:["I'm 24!", "I'm 24 years old."],
            2: ["My day was okay so far. A customer at the grocery shop I work at was being difficult and wanted to speak to the manager, but I managed to solve the problem after all!", "My day was okay so far. I had a presentation for a course of mine. I was really nervous, but people said it went really well! I’m relieved."],
            3:["Nice! So scenario" + self.scenariochoice + "We will practice that next session", "Great! So scenario" + self.scenariochoice + "I look forward to practicing that next time", "Sounds good! So scenario" + self.scenariochoice + "I can’t wait to practice it with you", "Alright! We will practice scenario" + scenariochoice + "the next session then. I bet you’ll do great"]}
